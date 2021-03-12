975c771bec58d21afcc6cc36cb8683d5de43e521
Armand Sallé <armand.salle@gmail.com>
2021-03-12 11:41:40 +0100

    remove unnecessary routes

--- pages/api/greetings/index.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-const handler = (req: NextApiRequest, res: NextApiResponse): void => {
-  res.status(404).json({ status: 404, message: `Route not found` })
-}
-
-export default handler
